8bfe230c8ff71df927617edc5ef1460be299fcd6
HarshitKachhadiya <harshit.kachhadiya18815@marwadieducation.edu.in>
2025-02-10 22:37:56 +0530

    `Added IIFE wrapper and commented out code to index.js`

--- basics/calculate/index.js
+++ basics/calculate/index.js
@@ -2,3 +2,18 @@ const {calculateSubstraction} = require("./substract");
 const {calculateSum} = require("./sum");
 
 module.exports = {calculateSubstraction, calculateSum};
+
+// require("./path");
+// All the code of the module wrapped inside the function(IIFE)
+// IIFE - Immediately invoked function expression 
+
+// (function (module,require,....) {
+// //     // All code of the module runs inside here
+        // require("./path")
+//     function calculateSubstraction(a,b){
+//         const substraction = a - b;
+//         console.log(substraction); 
+//     }
+//     module.exports = {calculateSubstraction}
+    
+// })(module.exports={});
